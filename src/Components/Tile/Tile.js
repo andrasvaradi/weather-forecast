d7a2f8778242b0a6f43510ee2f4f4c78cf618ae9
Andras <65424110+andrasium@users.noreply.github.com>
2021-05-06 19:38:07 +0100

    feat: display appropriate weather information in Tiles component

--- src/Components/Tile/Tile.js
+++ src/Components/Tile/Tile.js
@@ -1,15 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import './tile.css'
 import moment from 'moment';
+import ReactAnimatedWeather from 'react-animated-weather';
 
-const Tile = ({data}) => {
+const Tile = ({ data }) => {
+
+  const displayWeatherIcon = () => {
+    switch(data.weather[0].main) {
+      
+      case 'Clouds':
+        return 'CLOUDY'
+      case 'Rain':
+        return 'RAIN'
+      case 'Drizzle':
+        return 'RAIN'
+      case 'Snow':
+        return 'SNOW'
+      case 'Thunderstorm':
+        return 'Sleet'
+      case 'Fog':
+        return 'FOG'
+      default:
+        return 'CLEAR_DAY'
+    }
+  }
 
   return (
     <div className="tile">
-      
+
       <div>{moment(data.dt_txt).format('dddd')}</div>
       <div>{moment(data.dt_txt).format('LT')}</div>
-      <div></div>
+      <div>{data.weather[0].main}</div>
+      <ReactAnimatedWeather
+        icon={displayWeatherIcon()}
+        color={'goldenrod'}
+        size={20}
+        animate={true}
+      />
+      <div>{Math.floor(data.main.temp - 273.15)} C</div>
     </div>
   )
 }
